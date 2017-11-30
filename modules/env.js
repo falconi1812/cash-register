0d6e531fe3120b2c2cbc56d22d633358740ee850
Renato Moor <renatomoor1@gmail.com>
2017-11-30 21:12:40 +0100

    Creating config-products.html

--- modules/env.js
+++ modules/env.js
@@ -2,4 +2,5 @@
 const MAIN = "https://api-paintball.herokuapp.com";
 
 const CLIENTS = MAIN + "/clients/2017-10-28";
-const LOCATIONS = MAIN + "/locations/{locationid}"
+const LOCATIONSGET = MAIN + "/locations/{locationid}";
+const LOCATIONSPUT  = MAIN + "/locations/products/{location_code}/{product_id}";
